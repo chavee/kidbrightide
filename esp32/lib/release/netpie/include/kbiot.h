8b834c5ef150473304534c7eea19dc5a23cd6a22
chavee <i@chavee.com>
2018-07-01 02:33:49 +0700

    update libnetpie

--- esp32/lib/release/netpie/include/kbiot.h
+++ esp32/lib/release/netpie/include/kbiot.h
@@ -13,17 +13,23 @@ extern "C" {
 
 typedef enum {
     KBIOT_BUTTON_RELEASED = 0,
-    KBIOT_BUTTON_PRESSED,
-    KBIOT_BUTTON_CLICKED,
+    KBIOT_BUTTON_PRESSED = 1,
+    KBIOT_BUTTON_CLICKED = 2,
 } kbiot_button_state_t;
-
 
 void kbiot_init(char* kbserial, char *clientid, char *username, char *password);
 void kbiot_setValue_int(char *target, int value);
 void kbiot_setValue_str(char *target, char *value);
+void kbiot_writefeed_int(char *appid, char *feed, int value);
 void kbiot_setConfig(char *target, char *data);
-
-
+void set_B1release();
+void set_B2release();
+uint8_t get_B1state();
+uint8_t get_B2state();
+uint8_t get_B1stateClicked();
+uint8_t get_B2stateClicked();
+// uint8_t iot_button_mapped(char * text, int sw_num);
+// uint8_t iot_button_read(int sw_num);
 #endif
 
 #ifdef __cplusplus
