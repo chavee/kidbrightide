af9c91a8e42164e7d1dc7bc2848dbac652e03447
zCaesar <suphawat@nexpie.com>
2018-07-16 16:14:55 +0700

    add esptool

--- gulpfile.js
+++ gulpfile.js
@@ -13,18 +13,20 @@ const pathy = path.join(pathx, '*.{tar,tar.bz2,tar.gz,zip}')
 const patha = path.join(__dirname, 'app', 'package.json')
 const pathc = path.join(__dirname, 'package.json')
 const pathk = path.join(__dirname, 'archive', 'kidbright32.json')
-var pathl
+const pathcz = path.join(__dirname, '*.{tar,tar.bz2,tar.gz,zip}')
+var pathl, pathe
 
 // clean
 const pathcx = path.join(__dirname, 'xtensa-esp32-elf')
 const pathca = path.join(__dirname, 'app', 'node_modules')
 const pathcc = path.join(__dirname, 'node_modules')
 
-gulp.task('download', () => {
+gulp.task('download_xtensa', () => {
     var json = JSON.parse(fs.readFileSync(pathk))
     for (var i = 0; i < json.packages.length; i++) {
         if (process.platform == json.packages[i].platform) {
             pathl = json.packages[i].items[0].download_url
+            pathe = json.packages[i].items[1].download_url
             break
         }
     }
@@ -37,7 +39,18 @@ gulp.task('download', () => {
     }
 })
 
-gulp.task('decompress', ['download'], () => {
+gulp.task('download_esptool', ['download_xtensa'], () => {
+    return download(pathe)
+        .pipe(gulp.dest(__dirname))
+})
+
+gulp.task('decompress_esptool', ['download_esptool'], () => {
+    return gulp.src(pathcz)
+        .pipe(decompress({ strip: 1 }))
+        .pipe(gulp.dest(__dirname))
+})
+
+gulp.task('decompress_xtensa', ['decompress_esptool'], () => {
     if (fs.existsSync(pathl)) {
         return
     }
@@ -48,8 +61,8 @@ gulp.task('decompress', ['download'], () => {
     }
 })
 
-gulp.task('del', ['decompress'], () => {
-    return del([pathy])
+gulp.task('del', ['decompress_xtensa'], () => {
+    return del([pathy, pathcz])
 })
 
 gulp.task('install', ['del'], () => {
