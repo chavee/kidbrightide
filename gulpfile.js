74b824ffe1fcc7805c4b7f3d87b42771439a3bb2
zCaesar <suphawat@nexpie.com>
2018-07-24 09:55:44 +0700

    build work

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp')
 const path = require('path')
 const download = require("gulp-download")
-const decompress = require('gulp-decompress')
 const del = require('del')
 const install = require("gulp-install")
 const exec = require('gulp-exec');
@@ -9,12 +8,12 @@ const fs = require('fs')
 
 // build
 const pathx = path.join(__dirname, 'xtensa-esp32-elf')
-const pathy = path.join(pathx, '*.{tar,tar.bz2,tar.gz,zip}')
 const patha = path.join(__dirname, 'app', 'package.json')
 const pathc = path.join(__dirname, 'package.json')
 const pathk = path.join(__dirname, 'archive', 'kidbright32.json')
-const pathcz = path.join(__dirname, '*.{tar,tar.bz2,tar.gz,zip}')
+const pathz = path.join(__dirname, '*.{tar,tar.bz2,tar.gz,zip}')
 var pathl, pathe
+var pathnl, pathne, pathnlt, pathnet
 
 // clean
 const pathcx = path.join(__dirname, 'xtensa-esp32-elf')
@@ -26,7 +25,13 @@ gulp.task('download_xtensa', () => {
     for (var i = 0; i < json.packages.length; i++) {
         if (process.platform == json.packages[i].platform) {
             pathl = json.packages[i].items[0].download_url
+            pathnl = pathl.split("dl/")[1]
+            pathnlt = pathnl.split('.gz')[0]
             pathe = json.packages[i].items[1].download_url
+            if (process.platform == 'win32' || process.platform == 'darwin') {
+                pathne = pathe.split("dl/")[1]
+                pathnet = pathne.split('.gz')[0]
+            }
             break
         }
     }
@@ -35,7 +40,7 @@ gulp.task('download_xtensa', () => {
     }
     else {
         return download(pathl)
-            .pipe(gulp.dest(pathx))
+            .pipe(gulp.dest(__dirname))
     }
 })
 
@@ -44,35 +49,36 @@ gulp.task('download_esptool', ['download_xtensa'], () => {
         .pipe(gulp.dest(__dirname))
 })
 
-gulp.task('decompress_esptool', ['download_esptool'], () => {
-    return gulp.src(pathcz)
-        .pipe(decompress({ strip: 1 }))
-        .pipe(gulp.dest(__dirname))
-})
-
-gulp.task('chmod_linux', ['decompress_esptool'], () => {
-    if (process.platform == 'linux') {
-        return gulp.src(__dirname)
-            .pipe(exec('chmod 755 esptool.py'))
-    } 
-    else {
-        return
-    }
-})
-
-gulp.task('decompress_xtensa', ['chmod_linux'], () => {
+gulp.task('decompress', ['download_esptool'], () => {
     if (fs.existsSync(pathl)) {
         return
     }
     else {
-        return gulp.src(pathy)
-            .pipe(decompress({ strip: 2 }))
-            .pipe(gulp.dest(pathx))
+        if (process.platform == 'linux' || process.platform == 'darwin') {
+            return gulp.src(__dirname)
+                .pipe(exec('gunzip *.gz'))
+                .pipe(exec('tar xvf ' + pathnlt))
+                .pipe(exec('tar xvf ' + pathnet))
+        } else if (process.platform == 'win32') {
+            return gulp.src(__dirname)
+                .pipe(exec("powershell.exe -NoP -NonI -Command " + "Expand-Archive '"+ pathnl + "'"))
+                .pipe(exec("powershell.exe -NoP -NonI -Command " + "Expand-Archive '"+ pathne + "'"))
+        }
     }
 })
 
-gulp.task('del', ['decompress_xtensa'], () => {
-    return del([pathy, pathcz])
+gulp.task('chmod_linux', ['decompress'], () => {
+    if (process.platform == 'linux') {
+        return gulp.src(__dirname)
+            .pipe(exec('chmod 755 esptool.py'))
+    }
+    else {
+        return
+    }
+})
+
+gulp.task('del', ['chmod_linux'], () => {
+    return del([pathz])
 })
 
 gulp.task('install', ['del'], () => {
@@ -81,7 +87,7 @@ gulp.task('install', ['del'], () => {
 })
 
 gulp.task('build', ['install'], () => {
-    return 
+    return
 })
 
 gulp.task('clean', () => {
