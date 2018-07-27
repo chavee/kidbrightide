23f8afc068e1cd03c7686da82a821b19655f72aa
chavee <i@chavee.com>
2018-07-28 01:30:03 +0700

    update libnetpie

--- esp32/lib/release/netpie/include/kbiot.h
+++ esp32/lib/release/netpie/include/kbiot.h
@@ -11,6 +11,14 @@ extern "C" {
 
 #define TAG "KBIOT"
 
+// token bucket parameters
+#define FEED_BUCKETSIZE 4
+#define FEED_FILLRATE   0.135
+#define PUB_BUCKETSIZE 6
+#define PUB_FILLRATE   0.51
+
+#define KBIOT_DEBUG 1 
+
 typedef enum {
     KBIOT_BUTTON_RELEASED = 0,
     KBIOT_BUTTON_PRESSED = 1,
@@ -23,6 +31,8 @@ void kbiot_setValue_str(char *target, char *value);
 void kbiot_writeFeed(char *feedid, char *key, float value);
 void kbiot_setValue_float(char *target, float value);
 void kbiot_setConfig(char *target, char *data);
+bool toPublish(Microgear *mg, char *topic, char *payload, int len, int qos, int retain);
+void toWriteFeed(Microgear *mg, char *topic, char *payload, int len, int qos, int retain);
 void set_B1release();
 void set_B2release();
 uint8_t get_B1state();
